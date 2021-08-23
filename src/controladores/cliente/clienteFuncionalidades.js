1eae6ed09ab0e36bb37ca4221307fd6214a8d87d
Rafael Lasserre <80365790+Rlasserre@users.noreply.github.com>
2021-08-23 13:06:56 -0300

    Update clienteFuncionalidades.js
    adicionado cardapio restaurantes

--- src/controladores/cliente/clienteFuncionalidades.js
+++ src/controladores/cliente/clienteFuncionalidades.js
@@ -33,7 +33,27 @@ const buscarRestaurantes = async (req, res) => {
 }
 
 const verCardapioRestaurante = async (req, res) => {
+	const { id } = req.params;
+	try {
+		const restaurante = await knex
+			.select('*')
+			.from('restaurante')
+			.where({ usuario_id: id })
+			.first();
 
+		const imagemCategoria = await knex
+			.column({ img_categoria: 'imagem' })
+			.select()
+			.from('categoria')
+			.where({ id: restaurante.categoria_id })
+			.first();
+
+		restaurante.img_categoria = imagemCategoria.img_categoria;
+
+		return res.status(200).json(restaurante);
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 const detalharProdutoRestaurante = async (req, res) => {
